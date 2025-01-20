5eb3418c9ce16bdded3fd1b2a903349cd6216e93
Sadik-Sami <miasimanto@gmail.com>
2025-01-20 07:03:53 +0600

    Added query client and wrapped the app with the provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Router';
 import { ThemeProvider } from './providers/theme-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5, // 5 minutes
+			retry: 1,
+		},
+	},
+});
 createRoot(document.getElementById('root')).render(
-	<ThemeProvider>
-		<RouterProvider router={router} />
-	</ThemeProvider>
+	<QueryClientProvider client={queryClient}>
+		<ThemeProvider>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</QueryClientProvider>
 );
