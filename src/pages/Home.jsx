652d55312a6c256a498b2f7b2906b5d604614543
Sadik-Sami <miasimanto@gmail.com>
2025-02-18 03:18:39 +0600

    repositioned homepage components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,60 @@
-import AwardsSection from '@/components/Home/AwardsSection';
-import CategorySection from '@/components/Home/CategorySection';
-import FAQSection from '@/components/Home/FAQSection';
-import FeaturedPublishers from '@/components/Home/FeaturedPublisher';
 import Hero from '@/components/Home/Hero';
-import PartnersSection from '@/components/Home/PartnersSection';
-import PlansSection from '@/components/Home/PlansSection';
-import StatsSection from '@/components/Home/StatsSection';
-import SubscriptionAd from '@/components/Home/SubscriptionAd';
-import TestimonialsSection from '@/components/Home/TestimonialsSection';
+import CategorySection from '@/components/Home/CategorySection';
 import TrendingArticles from '@/components/Home/TrendingArticles';
-import React from 'react';
+import StatsSection from '@/components/Home/StatsSection';
+import TestimonialsSection from '@/components/Home/TestimonialsSection';
+import FeaturedPublishers from '@/components/Home/FeaturedPublisher';
+import PlansSection from '@/components/Home/PlansSection';
+import AwardsSection from '@/components/Home/AwardsSection';
+import PartnersSection from '@/components/Home/PartnersSection';
+import FAQSection from '@/components/Home/FAQSection';
+import SubscriptionAd from '@/components/Home/SubscriptionAd';
 
 const Home = () => {
 	return (
-		<div>
+		<div className='flex flex-col'>
+			{/* Initial Impact & Value Proposition */}
 			<Hero />
-			<CategorySection />
-			<PlansSection />
-			<TrendingArticles />
-			<StatsSection />
-			<TestimonialsSection />
-			<FeaturedPublishers />
-			<AwardsSection />
-			<PartnersSection />
-			<FAQSection />
-			<SubscriptionAd />
+			<div className='bg-background'>
+				<CategorySection />
+			</div>
+
+			{/* Content Value */}
+			<div className='bg-muted/30'>
+				<TrendingArticles />
+			</div>
+
+			{/* Social Proof & Credibility */}
+			<div className='bg-background'>
+				<StatsSection />
+			</div>
+			<div className='bg-muted/30'>
+				<TestimonialsSection />
+			</div>
+			<div className='bg-background'>
+				<FeaturedPublishers />
+			</div>
+
+			{/* Conversion Focus */}
+			<div className='bg-muted/30'>
+				<PlansSection />
+			</div>
+
+			{/* Additional Social Proof */}
+			<div className='bg-background'>
+				<AwardsSection />
+			</div>
+			<div className='bg-muted/30'>
+				<PartnersSection />
+			</div>
+
+			{/* Trust Building & Final CTA */}
+			<div className='bg-background'>
+				<FAQSection />
+			</div>
+			<div className='bg-primary/5'>
+				<SubscriptionAd />
+			</div>
 		</div>
 	);
 };
